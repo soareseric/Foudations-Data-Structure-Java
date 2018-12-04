415d69a514d7bc9fbd87586297ccc96dcf0a9580
soareseric <contato.eric.sm@gmail.com>
2018-12-04 15:27:26 -0200

    Criando a estrutura basica de uma lista ligada

---
+++ src/com/ericsoares/datastructure/listasligadas/ListaLigada.java
@@ -0,0 +1,17 @@
+package com.ericsoares.datastructure.listasligadas;
+
+public class ListaLigada<T> {
+
+	private No<T> primeiroNo;
+	private No<T> ultimoNo;
+	private int tamanho;
+	
+	public ListaLigada() {
+		this.primeiroNo = null;
+		this.ultimoNo = null;
+	}
+	
+	
+	
+	
+}
