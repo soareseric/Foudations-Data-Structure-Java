bfe661dfedd161a52143ee2925328f3364d1c7c0
soareseric <contato.eric.sm@gmail.com>
2018-12-04 17:29:20 -0200

    Inicio implementacao do metodo toString e conhecendo o StringBuilder

--- src/com/ericsoares/datastructure/listasligadas/ListaLigada.java
+++ src/com/ericsoares/datastructure/listasligadas/ListaLigada.java
@@ -1,17 +1,16 @@
 package com.ericsoares.datastructure.listasligadas;
 
 public class ListaLigada<T> {
-
+	
 	private No<T> primeiroNo;
 	private No<T> ultimoNo;
 	private int tamanho;
 	
 	public ListaLigada() {
 		this.primeiroNo = null;
-		this.ultimoNo = null;
+		this.ultimoNo = null;	
 		this.tamanho = 0;
 	}
-	
 	
 	public void inserir(T elemento) {
 		No<T> novoNo = new No<T>(elemento);
@@ -32,6 +31,21 @@ public class ListaLigada<T> {
 	public int tamanho() {
 		return this.tamanho;
 	}
-	
+
+	@Override
+	public String toString() {
+		if (estaVazia()) {
+			return "ListaLigada []";
+		} else {
+			No<T> noInicial = this.primeiroNo;
+			StringBuilder sb = new StringBuilder();
+			sb.append("Lista [");
+			sb.append(noInicial.getElemento() != null ? noInicial.getElemento().toString() : "<NULO>");	
+			sb.append(",");
+			while (noInicial.getElemento() != null) {
+				sb.append(noInicial.getProximo().getElemento());
+			}
+		}
+	}
 	
 }
