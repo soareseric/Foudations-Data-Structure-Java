60d8fc48861ed381170fdfb1bdab63ebc2bc9a32
soareseric <contato.eric.sm@gmail.com>
2018-12-05 09:45:22 -0200

    Funcionalidade de insercao em uma posicao e recuperacao de nós

--- src/com/ericsoares/datastructure/listasligadas/ListaLigada.java
+++ src/com/ericsoares/datastructure/listasligadas/ListaLigada.java
@@ -12,6 +12,54 @@ public class ListaLigada<T> {
 		this.tamanho = 0;
 	}
 	
+	public void inserirEm(int posicao, T elemento) {
+		if (posicao >= tamanho) {
+			throw new IllegalArgumentException(String.format("Posição inválida [%d]", posicao));
+		}
+		
+		if (posicao == 0) {
+			No<T> novoNo = new No<T>(elemento);
+			novoNo.setProximo(this.primeiroNo);
+			this.primeiroNo = novoNo;
+			
+		} else if (posicao == this.tamanho() -1 ) {
+			No<T> novoNo = new No<T>(elemento);
+			this.ultimoNo.setProximo(novoNo);
+			this.ultimoNo = novoNo;
+			
+		} else {
+			No<T> noAnterior = recuperarNo(posicao - 1);
+			No<T> noAtual = recuperarNo(posicao);
+			No<T> novoNo = new No<>(elemento);
+			noAnterior.setProximo(novoNo);
+			novoNo.setProximo(noAtual);
+		}
+		this.tamanho++;
+	}
+	
+	public T recuperar(int posicao) {
+		No<T> no = recuperarNo(posicao);
+		if (no != null) {
+			return no.getElemento();
+		}
+		return null;
+	}
+	
+	private No<T> recuperarNo(int posicao) {
+		if (posicao >= tamanho()) {
+			throw new IllegalArgumentException(String.format("Posição inválida [%d]", posicao));
+		}
+		No<T> resultado = null;
+		for (int i = 0; i <= posicao; i++) {
+			if (i == 0) {
+				resultado = this.primeiroNo;
+			} else {
+				resultado = resultado.getProximo();
+			}
+		}
+			return resultado;
+	}
+	
 	public void inserir(T elemento) {
 		No<T> novoNo = new No<T>(elemento);
 		if (estaVazia()) {
